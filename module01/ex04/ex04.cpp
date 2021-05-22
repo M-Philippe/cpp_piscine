c887069af6be1236cdb04a2950b72d368683996b
M-Philippe <m.philippe59@gmail.com>
2021-05-22 13:18:46 +0200

    Removed unneccesary lines in mod01/ex04

--- module01/ex04/ex04.cpp
+++ module01/ex04/ex04.cpp
@@ -6,9 +6,6 @@ int main()
 	std::string str = "HI THIS IS BRAIN";
 	std::string *ptr = &str;
 	std::string &ref = str;
-	std::cout << &str << std::endl;
-	std::cout << &ptr << std::endl;
-	std::cout << &ref << std::endl;
 	std::cout << "Pointer: " << *ptr << "\n";
 	std::cout << "Reference: " << ref << "\n";
 }
